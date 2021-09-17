1cab4d18d34c4a1ee5b3f5eae4e61abe6824fb2f
Khanetsky-Zhenya <khanetsky.zhenya@mail.ru>
2021-09-17 10:27:01 +0300

    add:add solution task #1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import { Counter } from "./components/Counter";
+import { Button } from "./components/Button/Button";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{display:"flex"}}>
+        <Counter />
+        <Counter />
+        <Counter />
+        </div>
     </div>
   );
 }
 
 export default App;
+
+
+/*<Button
+        text={"primary"}
+        type={"primary"}
+        onClick={() => alert("Primary")}
+      />*/
---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import styles from './Counter.module.css';
+export class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+  onClickPlus = () => {
+    this.setState({ count: ++this.state.count });
+  };
+  onClickMinus = () => {
+    this.setState({ count: --this.state.count });
+  };
+  onClickReset = () => {
+    this.setState({ count:0});
+  };
+  render() {
+    return (
+      <div className={styles.counter}>
+        <p className={styles.counter_number}>{this.state.count}</p>
+        <div className={styles.all_button}>
+          <button className={styles.button__plus} onClick={this.onClickPlus}>+</button>
+          <button className={styles.button__res} onClick={this.onClickReset}><p className={styles.res}>&#8635;</p></button>
+          <button className={styles.button__minus} onClick={this.onClickMinus}>–</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+/*<button onClick={this.onClickMinus}>-</button>
+        <p style={{ fontSize: "10" }}>{this.state.count}</p>
+
+        {this.state.count !== 10 ? (
+          <button onClick={this.onClickPlus}>+</button>
+        ) : null}
+/*&#8635;
+        {this.state.count === 10 ? <p>достаточно</p> : null}*/
